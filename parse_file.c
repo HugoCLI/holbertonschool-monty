ce53312b8e764c193793492742ff4929d0e7b43a
RedginaldGodeau <5657@holbertonstudents.com>
2022-12-12 02:23:37 -0800

    adding  files

--- parse_file.c
+++ parse_file.c
@@ -6,13 +6,30 @@
  *
  * Return: Nothing.
  */
-void parse_file(FILE **file)
+int parse_file(FILE **file)
 {
         char *line = NULL;
+        char *cmd, av[1024];
+        int i = 0;
+
 	size_t len;
 
         while (getline(&line, &len, *file) > 0)
         {
                 printf("[%li] %s", strlen(line), line);
+                cmd = strtok(line, " ");
+                while (cmd != NULL)
+                {
+                        if (strlen(cmd) > 0)
+                        {
+                                av[i++] = cmd;
+                                printf("%s\n",token);
+                        }
+
+                        cmd = strtok(NULL, " ");
+                }
         }
+
+        
+
 }
