c50149936c32fbec42986b84fd753b4d769a45c7
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-12-12 01:36:19 -0800

    issue: when argument 1 is empty

--- main.c
+++ main.c
@@ -9,7 +9,14 @@
 
 int main(int argc, char *argv[])
 {
+	char *arguments[1024] = {NULL};
+	int file;
+	(void)argc;
 
-
+	if (!argv[1])
+	{
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
+		return (EXIT_FAILURE);
+	}
 	return (0);
 }
