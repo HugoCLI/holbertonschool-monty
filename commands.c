d773d56bd666e2ee56e61d839bb50fba665a72cd
Hugo Chilemme <hugo.chilemme@gmail.com>
2022-12-16 02:20:20 -0800

    Fix error merge

--- commands.c
+++ commands.c
@@ -2,33 +2,6 @@
 
 stack_t *array;
 
-  /**
-   * push_command - a function that execute commands
-   * @value: value
-   *
-   * Return: Nothing.
-   */
-  
-int push_command(int value)
-{
-        stack_t *new = malloc(sizeof(stack_t));
-
-        if (new == 0)
-        {
-                free(new);
-                return (-3);
-        }
-        new->prev = NULL;
-        new->next = NULL;
-        new->n = value;
-        if (array)
-            new->next = array;
-
-        array = new;          
-       
-  
-        return (1);
-  }
 /**
 * push_command - a function that execute commands
 * @value: value
@@ -61,6 +34,7 @@ int push_command(int value)
  */
 int pall_command(void)
 {
+        stack_t *cache;
 	if (array == NULL)
 		return (0);
 
@@ -110,57 +84,3 @@ int pop_command(void)
 
         return (1);
 }
-
-/**
- * swap_command - a function that execute commands
- *
- * Return: Nothing.
- */
-int swap_command(void)
-{
-        stack_t *first_stack;
-        stack_t *second_stack;
-
-	if (!array || !array->next)
-                return (7);
-
-        first_stack = array->next;
-        second_stack = array;
-
-        first_stack->prev = NULL;
-        second_stack->prev = first_stack;
-        second_stack->next = first_stack->next;
-        first_stack->next = second_stack;
-        
-        array = first_stack;
-        return (1);
-
-}
-
-/**
- * add_command - a function that execute commands
- *
- * Return: Nothing.
- */
-int add_command(void)
-{
-        int sum = 0;
-        stack_t *seek = array;
-        stack_t *cache;
-
-	if (!array || !array->next)
-                return (8);
-
-        sum += seek->n;
-        sum += seek->next->n;
-        cache = seek->next->next;
-
-        
-        free(array->next);
-        free(array);
-        array = cache;
-        push_command(sum);
-        
-        return (1);
-}
-
